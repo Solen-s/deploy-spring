80b6c0cf6972deca6d3abc702bbed248d3c5a36d
solen <solen0918@gmail.com>
2025-09-28 23:05:02 +0700

    updating v5

--- src/main/java/com/example/testingdeploy/command/DeployCommand.java
+++ src/main/java/com/example/testingdeploy/command/DeployCommand.java
@@ -20,7 +20,7 @@ public class DeployCommand {
     public DeployResponse deploy(String repoUrl, String branch) {
        List<String> logs = new ArrayList<>();
         try {
-            ProcessBuilder builder = new ProcessBuilder("bash", "./deploy.sh", repoUrl, branch);
+            ProcessBuilder builder = new ProcessBuilder("bash", "/home/solen/deploy-spring-project/deploy-spring/deploy.sh", repoUrl, branch);
             builder.directory(new File(System.getProperty("user.dir")));
             builder.redirectErrorStream(true);
             builder.environment().put("PATH", "/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin");
